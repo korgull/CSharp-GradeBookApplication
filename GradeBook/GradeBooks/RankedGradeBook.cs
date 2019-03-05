02aad99ece7ce62aeea9afbd0fb69d60d90fa290
Karl Fritz <fritz.karl@gmail.com>
2019-03-05 10:09:41 -0600

    done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,6 +34,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             base.CalculateStatistics();
+
         }
 
     }
